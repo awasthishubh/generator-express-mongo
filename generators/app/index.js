7ee0b3fc0adfcdcf901d651c3af238fd0950cb75
Shubham Awasthi <aw.shubh@gmail.com>
2019-03-12 03:43:34 +0530

    Generator: Added prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,18 +2,77 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-const path=require('path')
+const path = require('path')
+
+var npmPrompt = (defaultDest) => [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What\'s the name of Project',
+    default: defaultDest
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Little discription?',
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'Who\'s the author?',
+  },
+  {
+    type: 'input',
+    name: 'homepage',
+    message: 'Url of homepage?',
+  }
+]
+
+var ConfigPrompt = [
+  {
+    type: 'input',
+    name: 'port',
+    message: 'At what Port you want to run?',
+    default: 3000
+  },
+  {
+    type: 'confirm',
+    name: 'mongo',
+    message: 'Want to  use mongo?',
+    default: true
+  },
+  {
+    type: 'input',
+    when: function (response) {
+      return response.mongo;
+    },
+    name: 'mongoDB',
+    message: 'Enter full mongoDB URL?',
+  },
+  {
+    type: 'confirm',
+    name: 'cors',
+    message: 'Do you want to enable cores?',
+    default: false
+  }
+
+]
 
 module.exports = class extends Generator {
-  path(){
-    this.sourceRoot(path.join(__dirname,'..','templates'))
+  path() {
+    this.sourceRoot(path.join(__dirname, '..', 'templates'))
   }
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the super-duper ${chalk.red('generator-express-mongo')} generator!`)
     );
+    this.log(chalk.yellow("\n\nLet's configure NPM Package\n"))
+    var npm = await this.prompt(npmPrompt.bind(this)(this.destinationRoot().split('/').pop()))
+    this.log(chalk.yellow("\n\nLet's now configure project settings\n"))
+    var config = await this.prompt(ConfigPrompt)
 
+    return this.props={...npm,...config}
     const prompts = [
       {
         type: 'confirm',
@@ -24,7 +83,7 @@ module.exports = class extends Generator {
     ];
 
     return this.prompt(prompts).then(props => {
-      if(props.someAnswer){
+      if (props.someAnswer) {
         // this.composeWith(require.resolve('../sub-generators/env'), {preprocessor: 'sass'})
         // this.composeWith(require.resolve('../sub-generators/models'), {preprocessor: 'sass'});
         // this.composeWith(require.resolve('../sub-generators/routes'), {preprocessor: 'sass'});
@@ -34,7 +93,7 @@ module.exports = class extends Generator {
     });
   }
 
-  
+
   writing() {
     console.log(this.config.get("models"))
     this.fs.copy(
@@ -46,4 +105,4 @@ module.exports = class extends Generator {
   install() {
     this.installDependencies({ bower: false });
   }
-};
+}; 
